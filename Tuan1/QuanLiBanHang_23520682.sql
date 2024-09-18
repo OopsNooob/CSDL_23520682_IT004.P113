15db406db7391bb023e60ae8d75d55e0a48db2e0
Đỗ Đình Khang <dodinhkhang8@gmail.com>
2024-09-18 10:05:03 +0700

    Add files via upload

---
+++ Tuan1/QuanLiBanHang_23520682.sql
@@ -0,0 +1,89 @@
+﻿CREATE DATABASE QLKHACHHANG;
+DROP TABLE KHACHHANG
+DROP TABLE NHANVIEN
+DROP TABLE SANPHAM
+DROP TABLE HOADON
+DROP TABLE CTHD
+CREATE TABLE KHACHHANG
+(
+MAKH char(4) primary key,
+HOTEN varchar(40),
+DCHI varchar(50),
+SODT varchar(20),
+NGSINH smalldatetime,
+NGDK smalldatetime,
+DOANHSO money
+)
+
+CREATE TABLE NHANVIEN
+(
+MANV char(4) primary key,
+HOTEN varchar(40),
+SODT varchar(20),
+NGVL smalldatetime
+)
+
+CREATE TABLE SANPHAM
+(
+MASP char(4) primary key,
+TENSP varchar(40),
+DVT varchar(20),
+NUOCSX varchar(40),
+GIA money
+)
+
+CREATE TABLE HOADON
+(
+SOHD int primary key,
+NGHD smalldatetime,
+MAKH char(4) foreign key references KHACHHANG(MAKH),
+MANV char(4) foreign key references NHANVIEN(MANV),
+TRIGIA money
+)
+
+CREATE TABLE CTHD
+(
+SOHD int not null foreign key references HOADON(SOHD),
+MASP char(4) not null foreign key references SANPHAM(MASP),
+SL int
+)
+
+ALTER TABLE CTHD
+ADD primary key (SOHD, MASP)
+
+/** Thêm vào thuộc tính GHICHU có kiểu dữ liệu varchar(20) cho quan hệ SANPHAM.**/
+ALTER TABLE SANPHAM
+ADD GHICHU varchar(20)
+
+/**  Thêm vào thuộc tính LOAIKH có kiểu dữ liệu là tinyint cho quan hệ KHACHHANG**/
+ALTER TABLE KHACHHANG
+ADD LOAIKH tinyint
+
+/** Sửa kiểu dữ liệu của thuộc tính GHICHU trong quan hệ SANPHAM thành varchar(100)**/
+ALTER TABLE SANPHAM
+ALTER COLUMN GHICHU varchar(100)
+
+/** Xóa thuộc tính GHICHU trong quan hệ SANPHAM**/
+ALTER TABLE SANPHAM
+DROP COLUMN GHICHU
+
+/** Làm thế nào để thuộc tính LOAIKH trong quan hệ KHACHHANG có thể lưu các giá trị là: “Vang lai”, “Thuong xuyen”, “Vip”**/
+ALTER TABLE KHACHHANG
+ALTER COLUMN LOAIKH varchar(30)
+
+/** Đơn vị tính của sản phẩm chỉ có thể là (“cay”,”hop”,”cai”,”quyen”,”chuc”)**/
+ALTER TABLE SANPHAM
+ADD CHECK (DVT = 'Cay' OR DVT = 'Hop' OR DVT = 'Cai' OR DVT = 'Quyen' OR DVT = 'Chuc')
+
+/** Giá bán của sản phẩm từ 500 đồng trở lên**/
+ALTER TABLE SANPHAM
+ADD CHECK (GIA > 500)
+
+/** Mỗi lần mua hàng, khách hàng phải mua ít nhất 1 sản phẩm.**/
+ALTER TABLE CTHD
+ADD CHECK (SL > 0)
+
+/** Ngày khách hàng đăng ký là khách hàng thành viên phải lớn hơn ngày sinh của người đó.**/
+ALTER TABLE KHACHHANG
+ADD CHECK (NGDK > NGSINH)
+
---
+++ Tuan1/QuanLiGiaoVu_23520682.sql
@@ -0,0 +1,118 @@
+﻿CREATE DATABASE QLGIAOVU
+
+CREATE TABLE KHOA 
+(
+MAKHOA varchar(4) primary key,
+TENKHOA varchar(40),
+NGTLAP smalldatetime,
+TRGKHOA char(4)
+)
+
+CREATE TABLE MONHOC 
+(
+MAMH varchar(10) primary key,
+TENMH varchar(40),
+TCLT tinyint,
+TCTH tinyint,
+MAKHOA varchar(4) foreign key references KHOA(MAKHOA)
+)
+
+CREATE TABLE DIEUKIEN
+(
+MAMH varchar(10) not null foreign key references MONHOC(MAMH),
+MAMH_TRUOC varchar(10) not null foreign key references MONHOC(MAMH)
+)
+
+ALTER TABLE DIEUKIEN
+ADD primary key (MAMH, MAMH_TRUOC)
+
+CREATE TABLE GIAOVIEN
+(
+MAGV char(4) primary key,
+HOTEN varchar(40),
+HOCVI varchar(10),
+HOCHAM varchar(10),
+GIOITINH varchar(3),
+NGSINH smalldatetime,
+NGVL smalldatetime,
+HESO numeric(4,2),
+MUCLUONG money,
+MAKHOA varchar(4) foreign key references KHOA(MAKHOA)
+)
+
+CREATE TABLE LOP
+(
+MALOP char(3) primary key,
+TENLOP varchar(40),
+TRGLOP char(5),
+SISO tinyint,
+MAGVCN char(4) foreign key references GIAOVIEN(MAGV)
+)
+
+CREATE TABLE HOCVIEN 
+(
+MAHV char(5) primary key,
+HO varchar(40),
+TEN varchar(10),
+NGSINH smalldatetime,
+GIOITINH varchar(3),
+NOISINH varchar(40),
+MALOP char(3) foreign key references LOP(MALOP)
+)
+CREATE TABLE GIANGDAY
+(
+MALOP char(3) not null foreign key references LOP(MALOP),
+MAMH varchar(10) not null foreign key references MONHOC(MAMH),
+MAGV char(4) not null foreign key references GIAOVIEN(MAGV),
+HOCKY tinyint,
+NAM smallint,
+TUNGAY smalldatetime,
+DENNGAY smalldatetime
+)
+
+ALTER TABLE GIANGDAY
+ADD primary key (MALOP, MAMH, MAGV)
+
+CREATE TABLE KETQUATHI
+(
+MAHV char(5) not null foreign key references HOCVIEN(MAHV),
+MAMH varchar(10) not null foreign key references MONHOC(MAMH),
+LANTHI tinyint,
+NGTHI smalldatetime,
+DIEM numeric(4,2),
+KQUA varchar(10)
+)
+
+ALTER TABLE KETQUATHI
+ADD primary key (MAHV, MAMH)
+
+/** Thuộc tính GIOITINH chỉ có giá trị là “Nam” hoặc “Nu”.**/
+ALTER TABLE GIAOVIEN 
+ADD CHECK (GIOITINH = 'Nam' or GIOITINH = 'Nu')
+ALTER TABLE HOCVIEN
+ADD CHECK (GIOITINH = 'Nam' or GIOITINH = 'Nu')
+
+/** Điểm số của một lần thi có giá trị từ 0 đến 10 và cần lưu đến 2 số lẽ (VD: 6.22)**/
+ALTER TABLE KETQUATHI
+ADD CHECK (DIEM <= 10 AND DIEM >= 0)
+
+/** Kết quả thi là “Dat” nếu điểm từ 5 đến 10 và “Khong dat” nếu điểm nhỏ hơn 5**/
+UPDATE KETQUATHI
+SET KQUA = 'Dat'
+WHERE DIEM >=5
+
+UPDATE KETQUATHI
+SET KQUA = 'Khong dat' 
+WHERE DIEM < 5
+
+/** Học viên thi một môn tối đa 3 lần.**/
+ALTER TABLE KETQUATHI
+ADD CHECK (LANTHI <=3)
+
+/** Học kỳ chỉ có giá trị từ 1 đến 3.**/
+ALTER TABLE GIANGDAY
+ADD CHECK (HOCKY >= 1 AND HOCKY <=3)
+
+/** Học vị của giáo viên chỉ có thể là “CN”, “KS”, “Ths”, ”TS”, ”PTS”.**/
+ALTER TABLE GIAOVIEN
+ADD CHECK (HOCVI = 'CN' OR HOCVI = 'KS' OR HOCVI = 'ThS' OR HOCVI = 'TS' OR HOCVI = 'PTS')
